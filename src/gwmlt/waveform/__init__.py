f42b60bae43e914ce4a09e97bb9e3a4cc56b6c63
Sparsha Ray <sparsharay@gmail.com>
2026-07-10 02:28:12 +0530

    feat: added polarization projection to get strain

--- src/gwmlt/waveform/__init__.py
+++ src/gwmlt/waveform/__init__.py
@@ -3,7 +3,9 @@ Waveform Generation and Processing
 """
 
 from .generator import generate_waveform
+from .projector import project_waveform
 
 __all__ = [
     "generate_waveform",
+    "project_waveform"
 ]
---
+++ src/gwmlt/waveform/projector.py
@@ -0,0 +1,127 @@
+"""
+Project GW Polarizations onto Detectors
+"""
+
+from warnings import warn
+
+import numpy as np
+from fractions import Fraction
+from scipy.signal import resample_poly
+
+from pycbc.types.timeseries import TimeSeries
+from pycbc.detector import Detector, get_available_detectors
+from pycbc.filter import resample_to_delta_t
+
+from ..core.sources import BBHSystem
+from ..core.observatories import LVK, Decihertz
+from ..decihertz.detectors import IndIGO_D
+
+
+def project_waveform(
+    hp: TimeSeries,
+    hc: TimeSeries,
+    source: BBHSystem,
+    observatory: LVK | Decihertz,
+) -> dict[str, TimeSeries] :
+
+    """
+    Project the polarization timeseries onto the observatory detectors.
+
+    Parameters
+    ----------
+    hp : TimeSeries
+        The h_plus polarization timeseries.
+    hc : TimeSeries
+        The h_cross polarization timeseries.
+    source : BBHSystem
+        The source parameters of the binary black hole system.
+    observatory : LVK | Decihertz
+        The observatory parameters.
+
+    Returns
+    -------
+    dict[str, TimeSeries]
+        A dictionary containing the projected timeseries for each detector in the observatory.
+    """
+
+    hp = hp.copy()
+    hc = hc.copy()
+
+    projected_strains = {}
+    pycbc_detectors = get_available_detectors()
+    
+    for detector_str in observatory.active_ifos :
+        
+        # Check if the detector is available in PyCBC or is a custom Decihertz detector
+        if detector_str in pycbc_detectors :
+            det = Detector(detector_str)
+        elif detector_str == "IndIGO-D" :
+            det = IndIGO_D()
+        else :
+            raise NotImplementedError(
+                f"Detector {detector_str} is not implemented in PyCBC or the custom Decihertz detectors."
+            )
+        
+        # Project the waveform onto the detector
+        strain = det.project_wave(hp, hc, source.ra, source.dec, source.psi)
+
+        # ![WARNING] : This assumes long wavelength approximation which is not valid above ~333 Hz for IndIGO-D.
+        # The signal is first projected and then downsampled because the downsampling is not an linear operation.
+        # If the waveform itself is generated at IndIGO-D band, then projection creates aliasing artifacts.
+        
+        # If the strain is already at the desired sampling rate, no resampling is needed
+        if strain.sample_rate == observatory.sample_rate : projected_strains[detector_str] = strain
+        else : # Resample the projected strain to the desired sampling rate
+
+            sig_start_time = strain.start_time
+
+            # Padding
+            corrupt_datapoints = 256 # We need about 256 datapoints of padding in the final strain
+            zero_pad_seconds   = 256 / observatory.sample_rate
+            strain.append_zeros (int(zero_pad_seconds * strain.sample_rate))
+            strain.prepend_zeros(int(zero_pad_seconds * strain.sample_rate))
+
+            # Do as much with Butterworth as possible
+            resample_ratio    = strain.sample_rate / observatory.sample_rate
+            butterworth_ratio = max(1, int(2 ** np.floor(np.log2(resample_ratio))))
+
+            strain_butterworth = resample_to_delta_t(
+                strain, 
+                1.0/(strain.sample_rate/butterworth_ratio), 
+                method='butterworth'
+            )
+
+            # Polyphase resampling
+            polyphase_ratio = Fraction(
+                observatory.sample_rate / strain_butterworth.sample_rate
+            )
+            up   = polyphase_ratio.numerator
+            down = polyphase_ratio.denominator
+
+            if (up>8) and (polyphase_ratio<=1) :
+                # Try ldas downsampling if the scale up factor for polyphase is too high 
+                warn(
+                    f"Polyphase upsampling factor {up} is too high. Switching to ldas.\n"
+                    f"Consider changing internal waveform generation sample rate."
+                )
+                ldas_strain = resample_to_delta_t(
+                    strain_butterworth, 
+                    1.0/observatory.sample_rate, 
+                    method='ldas'
+                )
+                ldas_strain = ldas_strain[corrupt_datapoints:-corrupt_datapoints]
+                ldas_strain.corrupted_samples = 0
+                projected_strains[detector_str] = ldas_strain
+                    
+            else :
+                # Otherwise, use polyphase resampling
+                polyphase_strain = resample_poly(strain_butterworth, up, down)
+                polyphase_strain = polyphase_strain[corrupt_datapoints:-corrupt_datapoints]
+                polyphase_strain = TimeSeries(
+                    polyphase_strain, 
+                    delta_t=1.0/observatory.sample_rate, 
+                    epoch=sig_start_time
+                )
+                projected_strains[detector_str] = polyphase_strain
+
+    return projected_strains
