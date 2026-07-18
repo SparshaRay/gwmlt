87b34a3e804b9dcb6c1a82dbb7331c7de4214975
Sparsha Ray <sparsharay@gmail.com>
2026-07-19 01:37:17 +0530

    refactor: all observatories now inherit from a single parent

--- src/gwmlt/core/observatories.py
+++ src/gwmlt/core/observatories.py
@@ -7,32 +7,75 @@ from pathlib import Path
 
 import numpy as np
 
-from ..config import config
+from gwmlt.config import config
 
 
-# Detector Network Configurations -----------------------------------------------------------------
+# Detector Networks -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 
 @dataclass(frozen=True)
-class LVK :
+class Observatory :
 
     """
-    Ground-based LIGO-Virgo-KAGRA detector network configurations.
+    Base class for observatory network configurations.
+    
+    Parameters
+    ----------
+    noise_psds : list of str or Path or None
+        Target noise psd (e.g., [None, 'O4_gaus', Path('path/to/psd.npz')] etc.)
+        If Path, it should point to a npz file with 'freq' and 'psd' arrays,
+        or an hdf5 file with noise timeseries.
+    detector_list : list of str, or None, optional
+        Target detectors. Default is None.
+        i.e., select all available detectors for the specified PSD tag.
+    f_low : float
+        Lower bound of frequency range for all analyses (in Hz).
+    sample_rate : float
+        Sampling rate of the timeseries data (in Hz). 
+        Sets upper bound of frequency range for all analyses at the Nyquist frequency.
+    wf_gen_srate : float
+        Internal sampling rate for the TD waveform generation (in Hz).
+    """
+
+    noise_psds    : list[str | Path | None]
+    detector_list : list[str] | None = None
+    f_low         : float
+    sample_rate   : float
+    wf_gen_srate  : float
+
+    resolved_paths : dict[str, Path] = field(default_factory=dict, init=False, repr=True)
+
+    def __post_init__(self) -> None:
+        paths_map = _resolve_paths(self.noise_psds, self.detector_list)
+        object.__setattr__(self, "resolved_paths", paths_map)
+
+    @property
+    def active_detectors(self) -> list[str]:
+        return list(self.resolved_paths.keys())
+
+
+@dataclass(frozen=True)
+class GroundBased(Observatory) :
+
+    """
+    Ground-based detector network configurations.
 
     Parameters
     ----------
     noise_psds : list of str or Path or None
-        Target noise psd tag (e.g., [None, 'O4_gaus'] etc.)
-        If Path, it should point to a npz file with 'freq' and 'psd' arrays.
+        Target noise psd (e.g., [None, 'O4_gaus', Path('path/to/psd.npz')] etc.)
+        If Path, it should point to a npz file with 'freq' and 'psd' arrays,
+        or an hdf5 file with noise timeseries.
     detector_list : list of str, or None, optional
-        Target detectors (e.g., ['H1', 'L1'] etc.).
-        Default is None.
+        Target detectors (e.g., ['H1', 'L1'] etc.). Default is None, 
+        i.e., select all available detectors for the specified PSD tag.
     f_low : float, optional
-        Lower bound of frequency range for all analysis (in Hz).
-        Sets reference frequency for waveform generation.
+        Lower bound of frequency range for all analyses (in Hz).
+        Also sets reference frequency and starting frequency for waveform generation.
         Default is 20.0
     sample_rate : float, optional
         Sampling rate of the timeseries data (in Hz).
-        Sets upper bound of frequency range for all analysis at the Nyquist frequency.
+        Sets upper bound of frequency range for all analyses at the Nyquist frequency.
         Default is 4096.0
     wf_gen_srate : float, optional
         Internal sampling rate for the TD waveform generation (in Hz).
@@ -51,25 +94,13 @@ class LVK :
     Otherwise, `noise_psds` and `detector_list` must be broadcastable to a common shape.
     """
 
-    noise_psds   : list[str | Path | None]
-    detector_list     : list[str] | None = None
-    f_low        : float = 20.0
-    sample_rate  : float = 4096.0
-    wf_gen_srate : float = 4096.0
-
-    resolved_paths : dict[str, Path] = field(default_factory=dict, init=False, repr=True)
-
-    def __post_init__(self) :
-        paths_map = _resolve_paths(self.noise_psds, self.detector_list)
-        object.__setattr__(self, "resolved_paths", paths_map)
-
-    @property
-    def active_detectors(self) -> list[str] :
-        return list(self.resolved_paths.keys())
+    f_low         : float = 20.000
+    sample_rate   : float = 4096.0
+    wf_gen_srate  : float = 4096.0
 
 
 @dataclass(frozen=True)
-class Decihertz :
+class Decihertz(Observatory) :
 
     """
     Space-based decihertz detector configurations.
@@ -77,22 +108,25 @@ class Decihertz :
     Parameters
     ----------
     wf_duration : float
-        Duration of the waveform (in seconds).
+        Desired duration of the waveform (in seconds).
     noise_psds : list of str or Path or None
-        Target noise psd tag (e.g., ['S1_gaus'] etc.)
+        Target noise psd (e.g., ['S1_gaus'] etc.)
         If Path, it should point to a npz file with 'freq' and 'psd' arrays.
     detector_list : list of str, or None, optional
-        Target detectors (e.g., ['IndIGO-D'] etc.).
-        Default is None.
+        Target detectors (e.g., ['IndIGO-D'] etc.). Default is None, i.e., 
+        select all available detectors for the specified PSD tag.
     ecc_f_ref : float, optional
-        Reference frequency for eccentricity.
+        Reference frequency for eccentricity. Relevant for eccentric waveforms only.
+        This is different from the f_ref which set globally according to bilby PE settings.
         Default is 20.0
     f_low : float, optional
-        Lower bound of frequency range for all analysis (in Hz).
+        Lower bound of frequency range for all analyses (in Hz).
+        This is not the same f_start used for waveform generation, 
+        which is automatically determined from the desired waveform duration.
         Default is 1.0
     sample_rate : float, optional
         Sampling rate of the timeseries data (in Hz).
-        Sets upper bound of frequency range for all analysis at the Nyquist frequency.
+        Sets upper bound of frequency range for all analyses at the Nyquist frequency.
         Default is 20.0
     wf_gen_srate : float, optional
         Internal sampling rate for the TD waveform generation (in Hz).
@@ -112,28 +146,20 @@ class Decihertz :
     Otherwise, `noise_psds` and `detector_list` must be broadcastable to a common shape.
     """
 
-    wf_duration  : float
-    noise_psds   : list[str | Path | None]
-    detector_list     : list[str] | None = None
+    wf_duration  : float = field(kw_only=True)
     ecc_f_ref    : float = 20.0
     f_low        : float = 1.00
     sample_rate  : float = 20.0
     wf_gen_srate : float = 2560.0
 
-    resolved_paths : dict[str, Path] = field(default_factory=dict, init=False, repr=True)
-
-    def __post_init__(self) :
-        paths_map = _resolve_paths(self.noise_psds, self.detector_list)
-        object.__setattr__(self, "resolved_paths", paths_map)
-
-    @property
-    def active_detectors(self) -> list[str] :
-        return list(self.resolved_paths.keys())
-
 
 # Helper function to resolve noise psd paths for given detectors ----------------------------------
+# -------------------------------------------------------------------------------------------------
 
-def _resolve_paths(noise_psds: list[str | Path | None], detector_list: list[str]) -> dict[str, Path] :
+def _resolve_paths(
+    noise_psds: list[str | Path | None], 
+    detector_list: list[str] | None
+) -> dict[str, Path] :
 
     """
     Resolve the noise psd paths for the given detectors.
@@ -151,8 +177,8 @@ def _resolve_paths(noise_psds: list[str | Path | None], detector_list: list[str]
 
     # Wrap single values in lists for user convenience, because python does not care about type hints anyways
     # Bad practice, but oh well, saves a second
-    noise_psds     = noise_psds if isinstance(noise_psds, list) else [noise_psds]
-    available_detectors = detector_list   if isinstance(detector_list,   list) else [detector_list  ]
+    noise_psds          = noise_psds    if isinstance(noise_psds,    list) else [noise_psds]
+    available_detectors = detector_list if isinstance(detector_list, list) else [detector_list]
 
     default_noise_psds = config.noise.noise_psds
 
