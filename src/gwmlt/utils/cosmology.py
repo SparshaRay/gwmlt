de0c74594a86a00e806f4537e410167da0d9e0eb
Sparsha Ray <sparsharay@gmail.com>
2026-07-27 22:24:16 +0530

    feat: added SFR and merger rate PDFs in cosmology

--- src/gwmlt/utils/cosmology.py
+++ src/gwmlt/utils/cosmology.py
@@ -5,10 +5,14 @@ Cosmology Utilities
 import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import brentq
+from scipy.interpolate import interp1d
 
 from gwmlt.constants import C_SI
 from gwmlt.config import config
 
+
+# Basic functions ---------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 
 def inverse_ez(z: float) -> float :
 
@@ -93,24 +97,6 @@ def transverse_comoving_distance(z: float) -> float :
         return (HUBBLE_DISTANCE_MPC / sqrt_ok) * np.sin(sqrt_ok * dc / HUBBLE_DISTANCE_MPC)
 
 
-def angular_diameter_distance(z: float) -> float :
-
-    """
-    Returns the angular diameter distance to redshift z.
-    
-    Reference: 
-        - Eq. 18, Hogg (2000)
-    
-    Parameters:
-        z (float): Redshift.
-    
-    Returns:
-        float: Angular diameter distance in Mpc.
-    """
-    
-    return transverse_comoving_distance(z) / (1.0 + z)
-
-
 def angular_diameter_distance_between(z1: float, z2: float) -> float :
 
     """
@@ -140,24 +126,6 @@ def angular_diameter_distance_between(z1: float, z2: float) -> float :
     term2 = dm1 * np.sqrt(1.0 + OMEGA_K * (dm2 / dh)**2)
     
     return (term1 - term2) / (1.0 + z2)
-
-
-def luminosity_distance(z: float) -> float :
-
-    """
-    Returns the luminosity distance to redshift z.
-    
-    Reference: 
-        - Eq. 21, Hogg (2000)
-    
-    Parameters:
-        z (float): Redshift.
-    
-    Returns:
-        float: Luminosity distance in Mpc.
-    """
-
-    return (1.0 + z) * transverse_comoving_distance(z)
 
 
 def comoving_volume(z: float) -> float :
@@ -193,6 +161,143 @@ def comoving_volume(z: float) -> float :
         return term1 * (term2 - (1.0 / sqrt_ok) * np.arcsinh(sqrt_ok * dm / dh))
     if OMEGA_K < 0.0 :
         return term1 * (term2 - (1.0 / sqrt_ok) * np.arcsin(sqrt_ok * dm / dh))
+
+
+def differential_comoving_volume(z: float) -> float :
+
+    """
+    Computes the differential comoving volume element dVc/dz in Gpc^3.
+
+    Parameters:
+        z (float): Redshift.
+
+    Returns:
+        float: Differential comoving volume element (Gpc^3 per unit redshift).
+    """
+
+    HUBBLE_CONSTANT = config.cosmology.HUBBLE_CONSTANT
+    HUBBLE_DISTANCE_MPC = C_SI / (HUBBLE_CONSTANT * 1000.0) 
+
+    D_A = angular_diameter_distance_between(0.0, z)
+    inv_Ez = inverse_ez(z)
+
+    # 4 * pi * (c/H0) * (1+z)^2 * D_A^2 / E(z)
+    # 1e-9 converts Mpc^3 to Gpc^3
+    d_Omega = 4.0 * np.pi
+    dVc_dz = (
+        d_Omega
+        * HUBBLE_DISTANCE_MPC
+        * ((1.0 + z) ** 2)
+        * (D_A ** 2)
+        * inv_Ez
+    ) * 1e-9
+
+    return dVc_dz
+
+
+# SFR and merger rates ----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
+
+def madau_dickinson_fishbach_sfr(z: float | np.ndarray) -> float | np.ndarray :
+
+    """
+    Star formation rate following Fishbach et al. (2018) [https://arxiv.org/abs/1805.10270],
+    based on Madau & Dickinson (2014) [https://arxiv.org/abs/1403.0007].
+
+    Parameters:
+        z (float or np.ndarray): Redshift.
+
+    Returns:
+        float or np.ndarray: Star formation rate in Gpc^-3 yr^-1
+    """
+
+    return 0.015 * ((1.0 + z) ** 2.7) / (1.0 + ((1.0 + z) / 2.9) ** 5.6)
+
+
+def madau_fragos_sfr(z: float | np.ndarray) -> float | np.ndarray :
+
+    """
+    Star formation rate following Madau & Fragos (2016) [https://arxiv.org/abs/1606.07887].
+
+    Parameters:
+        z (float or np.ndarray): Redshift.
+
+    Returns:
+        float or np.ndarray: Star formation rate in Gpc^-3 yr^-1
+    """
+
+    return 0.010 * ((1.0 + z) ** 2.6) / (1.0 + ((1.0 + z) / 3.2) ** 6.2)
+
+
+def merger_rate_density(
+    z: float, 
+    sfr_model: callable = madau_dickinson_fishbach_sfr
+) -> float :
+
+    """
+    The merger rate density (per unit redshift) in the detector frame :
+    dR_det/dz = [R_src(z) / (1 + z)] * (dVc/dz).
+
+    Parameters:
+        z (float): Redshift.
+        sfr_model (callable, optional): Function returning the source-frame star formation rate. 
+                                        Defaults to `madau_dickinson_fishbach_sfr`.
+
+    Returns:
+        float: Merger rate density in the detector frame.
+    """
+
+    rate_src = sfr_model(z)
+    dVc_dz = differential_comoving_volume(z)
+    return (rate_src * dVc_dz) / (1.0 + z)  # dt_det = dt_src * (1+z)
+
+
+def merger_rate_pdf_interpolator(
+    z_min: float, 
+    z_max: float, 
+    num_points: int = 10000,
+    sfr_model: callable = madau_dickinson_fishbach_sfr
+) -> interp1d :
+
+    """
+    Normalized probability density function (PDF) for merger rate in the detector frame.
+
+    Parameters:
+        z_min (float): Minimum redshift.
+        z_max (float): Maximum redshift.
+        num_points (int, optional): Number of grid points for interpolation. Defaults to 10000.
+        sfr_model (callable, optional): The source-frame star formation rate function. 
+                                        Defaults to `madau_dickinson_fishbach_sfr`.
+
+    Returns:
+        interp1d: A SciPy interpolation object representing the normalized PDF.
+    """
+
+    norm, _ = quad(merger_rate_density, z_min, z_max, args=(sfr_model,))
+    z_grid = np.linspace(z_min, z_max, num=num_points)
+    pdf_grid = np.array([merger_rate_density(z, sfr_model)/norm for z in z_grid])
+    return interp1d(z_grid, pdf_grid, kind='linear', bounds_error=False, fill_value=0.0)
+
+
+# Utility functions -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
+
+def luminosity_distance(z: float) -> float :
+
+    """
+    Returns the luminosity distance to redshift z.
+    
+    Reference: 
+        - Eq. 21, Hogg (2000)
+    
+    Parameters:
+        z (float): Redshift.
+    
+    Returns:
+        float: Luminosity distance in Mpc.
+    """
+
+    return (1.0 + z) * transverse_comoving_distance(z)
 
 
 def z_from_comoving_distance(target_dc: float, z_max: float = 20.0) -> float :
